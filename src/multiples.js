c0d304f017d19bd16bba1dc088c17502ae22c4f1
Victor Lee <myleevictor@gmail.com>
2018-03-09 10:14:31 +0800

    cleaned up code - removed one line in for loop

--- src/multiples.js
+++ src/multiples.js
@@ -1,13 +1,9 @@
 var result = [];
 
-function multiples(a,b){
-    for (let i = a; i <= b; i=i+a) {
-        if (i%a === 0){
-            result.push(i);
-        }
-    };
-
-return result;
+function multiples(a, b) {
+  for (let i = a; i <= b; i = i + a) {
+    result.push(i);
+  }
+  return result;
 }
-
 module.exports = multiples;
